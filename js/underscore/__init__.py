f618351c2cd326be49933ccc86b5f74f1f3941a4
Nick Bourgeois <nick@haxorize.com>
2012-09-18 12:04:13 -0500

    Added development version of underscore.js in addition to minified version

--- js/underscore/__init__.py
+++ js/underscore/__init__.py
@@ -1,4 +1,4 @@
 from fanstatic import Library, Resource
 
 library = Library('underscore.js', 'resources')
-underscore = Resource(library, 'underscore-min.js')
+underscore = Resource(library, 'underscore.js', minified='underscore-min.js')
