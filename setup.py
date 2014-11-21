58ac22761db59f58362a0c25e5961348a67137f1
Jan-Jaap Driessen <janjaapdriessen@gmail.com>
2014-11-21 15:19:04 +0100

    no hgtools

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(name='js.underscore',
       packages=find_packages(),namespace_packages=['js'],
       include_package_data=True,
       zip_safe=False,
-      setup_requires=['hgtools'],
       install_requires=['fanstatic',
                         'setuptools',],
       entry_points={'fanstatic.libraries': ['underscore.js = js.underscore:library',],},)
